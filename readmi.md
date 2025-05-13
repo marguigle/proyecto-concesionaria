be06b903e55831ea27ded1450933a4f428a9feaa
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2025-05-12 22:34:45 -0300

    primer commit

---
+++ readmi.md
@@ -0,0 +1,2 @@
+SItio web sobre una concesionaria de autos, con una landing page, un catalogo de autos, un formulario de contacto y una pagina de comentarios de loc clientes.
+hecha con html. css. y js.
---
+++ src/scripts/main.js
@@ -0,0 +1,16 @@
+import { autos } from "./autos.js";
+
+console.log(autos);
+
+const containerCard = document.getElementById("card-container");
+
+autos.forEach((auto) => {
+  return (containerCard.innerHTML += `
+       <div class="card">
+          <img src=${auto.foto} alt="foto-auto" />
+          <p>${auto.marca}</p>
+          <p>${auto.modelo}</p>
+          <p>${auto.segmento}</p>
+          <p>$ ${auto.precio}</p>
+        </div>`);
+});
